c92efb99c7c4163fb6fc25dafc137af3e335ebbd
Breynner Perez <breynnerper18@gmail.com>
2026-06-02 10:14:23 -0500

    Initial commit: Django portfolio with Tailwind CSS

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def projects(request):
+    return render(request, 'core/projects.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
